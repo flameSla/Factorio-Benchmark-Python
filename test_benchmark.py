16a7ca2560563e9343c4a921fe44752ae57c594c
flameSla <39356103+flameSla@users.noreply.github.com>
2022-08-31 19:32:04 +0700

    Update test_benchmark.py

--- test_benchmark.py
+++ test_benchmark.py
@@ -9,13 +9,38 @@ import benchmark
 # print("for: ", min(timeit.repeat(lambda: [float(i)/1000000.0 for i in a], repeat=10)))
 # print("map:", min(timeit.repeat(lambda: list(map(lambda i: float(i)/1000000.0, a)), repeat=10)))
 
+def check_dict(res, par, val):
+    if res[par] == val:
+        print(f'{par} ok')
+    else:
+        print(f'{par} bad')
 
-print(benchmark.get_avg('test_temp', 1, 5, 0, True))
+def check_list(res, par, val):
+    if res == val:
+        print(f'{par} ok')
+    else:
+        print(f'{par} bad')
+
+
+t1 = '1.1.64'
+t2 = [0.884, 0.821, 0.828, 0.823, 0.814]
+t3 = ['tick', 'timestamp', 'wholeUpdate', 'latencyUpdate', 'gameUpdate', 'circuitNetworkUpdate', 'transportLinesUpdate', 'fluidsUpdate', 'heatManagerUpdate', 'entityUpdate', 'particleUpdate', 'mapGenerator', 'mapGeneratorBasicTilesSupportCompute', 'mapGeneratorBasicTilesSupportApply', 'mapGeneratorCorrectedTilesPrepare', 'mapGeneratorCorrectedTilesCompute', 'mapGeneratorCorrectedTilesApply', 'mapGeneratorVariations', 'mapGeneratorEntitiesPrepare', 'mapGeneratorEntitiesCompute', 'mapGeneratorEntitiesApply', 'crcComputation', 'electricNetworkUpdate', 'logisticManagerUpdate', 'constructionManagerUpdate', 'pathFinder', 'trains', 'trainPathFinder', 'commander', 'chartRefresh', 'luaGarbageIncremental', 'chartUpdate', 'scriptUpdate']
+t4 = [4.9500000000000004e-05, 48.767019, 0.8829790000000002, 0.0, 0.874801, 7.900000000000006e-05, 0.0015100000000000003, 0.00034300000000000037, 0.0, 0.8183059999999999, 0.0, 0.0001289999999999998, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0002660000000000004, 0.0003480000000000003, 0.0004110000000000004, 0.0, 0.00024300000000000002, 0.0, 0.0002779999999999998, 0.0, 0.006994999999999998, 0.00021799999999999977, 0.000263] 
+
+res = benchmark.get_avg('test_temp', 1, 5, 0, True)
+print('---------------------')
+if isinstance(res, dict):
+    check_dict(res, 'version', t1)
+    check_dict(res, 'list_avgs', t2)
+    check_dict(res, 'column_names', t3)
+    check_dict(res, 'verbose_averaged_values', t4)
+elif isinstance(res, list):
+    check_list(res[0], 'version', t1)
+    check_list(res[1], 'list_avgs', t2)
+    check_list(res[2], 'column_names', t3)
+    check_list(res[3], 'verbose_averaged_values', t4)
+
+print('---------------------')
 print(benchmark.do_one_benchmark("template_for_benchmark", 100, 1, 5, 0, "", True))
 
-"""
-{'version': '1.1.64',
- 'list_avgs': [0.884, 0.821, 0.828, 0.823, 0.814],
- 'column_names': ['tick', 'timestamp', 'wholeUpdate', 'latencyUpdate', 'gameUpdate', 'circuitNetworkUpdate', 'transportLinesUpdate', 'fluidsUpdate', 'heatManagerUpdate', 'entityUpdate', 'particleUpdate', 'mapGenerator', 'mapGeneratorBasicTilesSupportCompute', 'mapGeneratorBasicTilesSupportApply', 'mapGeneratorCorrectedTilesPrepare', 'mapGeneratorCorrectedTilesCompute', 'mapGeneratorCorrectedTilesApply', 'mapGeneratorVariations', 'mapGeneratorEntitiesPrepare', 'mapGeneratorEntitiesCompute', 'mapGeneratorEntitiesApply', 'crcComputation', 'electricNetworkUpdate', 'logisticManagerUpdate', 'constructionManagerUpdate', 'pathFinder', 'trains', 'trainPathFinder', 'commander', 'chartRefresh', 'luaGarbageIncremental', 'chartUpdate', 'scriptUpdate'],
- 'verbose_averaged_values': [4.9500000000000004e-05, 48.767019, 0.8829790000000002, 0.0, 0.874801, 7.900000000000006e-05, 0.0015100000000000003, 0.00034300000000000037, 0.0, 0.8183059999999999, 0.0, 0.0001289999999999998, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0002660000000000004, 0.0003480000000000003, 0.0004110000000000004, 0.0, 0.00024300000000000002, 0.0, 0.0002779999999999998, 0.0, 0.006994999999999998, 0.00021799999999999977, 0.000263]}
-""" 
+
