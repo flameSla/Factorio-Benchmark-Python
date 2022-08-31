1dd1d57cbb98fbe9625bf60b9131ccf1668aa082
flameSla <39356103+flameSla@users.noreply.github.com>
2022-08-31 19:33:36 +0700

    update test_benchmark.py

--- test_benchmark.py
+++ test_benchmark.py
@@ -1,4 +1,4 @@
-import timeit
+# import timeit
 import benchmark
 
 
@@ -15,6 +15,7 @@ def check_dict(res, par, val):
     else:
         print(f'{par} bad')
 
+
 def check_list(res, par, val):
     if res == val:
         print(f'{par} ok')
@@ -25,7 +26,7 @@ def check_list(res, par, val):
 t1 = '1.1.64'
 t2 = [0.884, 0.821, 0.828, 0.823, 0.814]
 t3 = ['tick', 'timestamp', 'wholeUpdate', 'latencyUpdate', 'gameUpdate', 'circuitNetworkUpdate', 'transportLinesUpdate', 'fluidsUpdate', 'heatManagerUpdate', 'entityUpdate', 'particleUpdate', 'mapGenerator', 'mapGeneratorBasicTilesSupportCompute', 'mapGeneratorBasicTilesSupportApply', 'mapGeneratorCorrectedTilesPrepare', 'mapGeneratorCorrectedTilesCompute', 'mapGeneratorCorrectedTilesApply', 'mapGeneratorVariations', 'mapGeneratorEntitiesPrepare', 'mapGeneratorEntitiesCompute', 'mapGeneratorEntitiesApply', 'crcComputation', 'electricNetworkUpdate', 'logisticManagerUpdate', 'constructionManagerUpdate', 'pathFinder', 'trains', 'trainPathFinder', 'commander', 'chartRefresh', 'luaGarbageIncremental', 'chartUpdate', 'scriptUpdate']
-t4 = [4.9500000000000004e-05, 48.767019, 0.8829790000000002, 0.0, 0.874801, 7.900000000000006e-05, 0.0015100000000000003, 0.00034300000000000037, 0.0, 0.8183059999999999, 0.0, 0.0001289999999999998, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0002660000000000004, 0.0003480000000000003, 0.0004110000000000004, 0.0, 0.00024300000000000002, 0.0, 0.0002779999999999998, 0.0, 0.006994999999999998, 0.00021799999999999977, 0.000263] 
+t4 = [4.9500000000000004e-05, 48.767019, 0.8829790000000002, 0.0, 0.874801, 7.900000000000006e-05, 0.0015100000000000003, 0.00034300000000000037, 0.0, 0.8183059999999999, 0.0, 0.0001289999999999998, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0002660000000000004, 0.0003480000000000003, 0.0004110000000000004, 0.0, 0.00024300000000000002, 0.0, 0.0002779999999999998, 0.0, 0.006994999999999998, 0.00021799999999999977, 0.000263]
 
 res = benchmark.get_avg('test_temp', 1, 5, 0, True)
 print('---------------------')
@@ -42,5 +43,3 @@ elif isinstance(res, list):
 
 print('---------------------')
 print(benchmark.do_one_benchmark("template_for_benchmark", 100, 1, 5, 0, "", True))
-
-
